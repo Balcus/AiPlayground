3771071b4ce4974943983728faee6b2a1241bb4f
balcus <balcus@balcus.balcus.local>
2025-04-20 13:50:04 +0300

    run endpoints done

--- AiPlayground.DataAccess/Repositories/RunRepository.cs
+++ AiPlayground.DataAccess/Repositories/RunRepository.cs
@@ -9,35 +9,67 @@ public class RunRepository : BaseRepository<Run>
     {
     }
 
-    public async Task<List<Run>> GetAllByPromptId(int promptId)
+    public async Task<List<Run>> GetAll()
     {
         try
         {
             return await _context
                 .Run
                 .Include(r => r.Prompt)
-                .Where(r => r.PromptId == promptId)
+                .Include(r => r.Model)
                 .ToListAsync();
         }
         catch (Exception e)
         {
-            throw new Exception($"Error retrieving all runs for prompt with ID {promptId}: {e.Message}", e);
+            throw new Exception($"Error retrieving all platforms with models: {e.Message}", e);
         }
     }
-
-    public async Task<List<Run>> GetAllByModelId(int modelId)
+    
+    public override async Task<Run?> GetByIdAsync(int id)
     {
         try
         {
             return await _context
                 .Run
-                .Include(r => r.Model)
-                .Where(r => r.ModelId == modelId)
-                .ToListAsync();
+                .Include(r =>r.Model)
+                .Include(r => r.Prompt)
+                .FirstOrDefaultAsync(p => p.Id == id);
         }
         catch (Exception e)
         {
-            throw new Exception($"Error retrieving all runs for model with ID {modelId}: {e.Message}", e);
+            throw new Exception($"Error retrieving platform with ID {id}: {e.Message}", e);
         }
     }
+
+    // public async Task<List<Run>> GetAllByPromptId(int promptId)
+    // {
+    //     try
+    //     {
+    //         return await _context
+    //             .Run
+    //             .Include(r => r.Prompt)
+    //             .Where(r => r.PromptId == promptId)
+    //             .ToListAsync();
+    //     }
+    //     catch (Exception e)
+    //     {
+    //         throw new Exception($"Error retrieving all runs for prompt with ID {promptId}: {e.Message}", e);
+    //     }
+    // }
+    //
+    // public async Task<List<Run>> GetAllByModelId(int modelId)
+    // {
+    //     try
+    //     {
+    //         return await _context
+    //             .Run
+    //             .Include(r => r.Model)
+    //             .Where(r => r.ModelId == modelId)
+    //             .ToListAsync();
+    //     }
+    //     catch (Exception e)
+    //     {
+    //         throw new Exception($"Error retrieving all runs for model with ID {modelId}: {e.Message}", e);
+    //     }
+    // }
 }
